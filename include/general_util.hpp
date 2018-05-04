9f0ce8669c572b24ad1742435b32025db2597344
ProgrammingIncluded <chencharlesh@gmail.com>
2018-05-04 14:01:45 -0700

    Added function call overhead analysis.

--- include/general_util.hpp
+++ include/general_util.hpp
@@ -7,6 +7,8 @@
  * Website: ProgrammingIncluded.github.io
 *******************************************/
 
+#include <vector>
+
 /**
  * Runs the given function multiple times and
  * returns the given type result.
@@ -18,4 +20,31 @@ T multirun(T (*func)(), unsigned int sampleCount) {
         accum += func();
     }
     return accum / sampleCount;
+}
+
+/**
+ * Same as multirun except with multiple outputs
+ **/
+template<typename T>
+std::vector<T> multirun_multiout(std::vector<T> (*func)(), unsigned int sampleCount) {
+    std::vector<T> accum;
+
+    if(sampleCount == 1)
+        return func();
+    else if(sampleCount <= 0)
+        return accum;
+    
+    accum = func();
+    for(unsigned int x = 0; x < sampleCount; ++x) {
+        std::vector<T> tempRes = func();
+        for(unsigned int i = 0; i < tempRes.size(); ++i) {
+            accum[i] += tempRes[i];
+        }
+    }
+
+    for(unsigned int i = 0; i < accum.size(); ++i) {
+        accum[i] /= sampleCount;
+    }
+
+    return accum;
 }
--- src/cpu_overhead.cpp
+++ src/cpu_overhead.cpp
@@ -8,12 +8,13 @@
 *******************************************/
 #include <iostream>
 #include <chrono>
+#include <vector>
 
 #include "platform_util.hpp"
 #include "general_util.hpp"
 
 // Number of samples to run each overhead function.
-unsigned int SAMPLE_SIZE = 1000000;
+unsigned int SAMPLE_SIZE = 100000000;
 
 // Number of times to run for loop
 // Stats will be generated based off that.
@@ -31,10 +32,74 @@ long double for_loop_overhead() {
     return start;
 }
 
+// Function to measure overhead procedure calls.
+int func_call_arg0() { return 0; }
+int func_call_arg1(int arg0) { return 0; }
+int func_call_arg2(int arg0, int arg1) { return 0; }
+int func_call_arg3(int arg0, int arg1, int arg2) { return 0; }
+int func_call_arg4(int arg0, int arg1, int arg2, int arg3) { return 0; }
+int func_call_arg5(int arg0, int arg1, int arg2, int arg3, int arg4) { return 0; }
+int func_call_arg6(int arg0, int arg1, int arg2, int arg3, int arg4, int arg5) { return 0; }
+int func_call_arg7(int arg0, int arg1, int arg2, int arg3, int arg4, int arg5, int arg6) { return 0; }
+
+std::vector<long double> function_call_overhead() {
+    std::vector<long double> result;
+
+    long double start = platu::now();
+    func_call_arg0();
+    start = (platu::now() - start);
+    result.push_back(start);
+
+    start = platu::now();
+    func_call_arg1(0);
+    start = (platu::now() - start);
+    result.push_back(start);
+
+    start = platu::now();
+    func_call_arg2(0, 0);
+    start = (platu::now() - start);
+    result.push_back(start);
+
+    start = platu::now();
+    func_call_arg3(0, 0, 0);
+    start = (platu::now() - start);
+    result.push_back(start);
+
+    start = platu::now();
+    func_call_arg4(0, 0, 0, 0);
+    start = (platu::now() - start);
+    result.push_back(start);
+
+    start = platu::now();
+    func_call_arg5(0, 0, 0, 0, 0);
+    start = (platu::now() - start);
+    result.push_back(start);
+
+    start = platu::now();
+    func_call_arg6(0, 0, 0, 0, 0, 0);
+    start = (platu::now() - start);
+    result.push_back(start);
+
+    start = platu::now();
+    func_call_arg7(0, 0, 0, 0, 0, 0, 0);
+    start = (platu::now() - start);
+    result.push_back(start);
+
+    return result;
+}
+
 // Main function
 int main(int argc, char **argv) {
-    // Output
+    // Output Loop Overhead
     std::cout << "Number of Runs: " << SAMPLE_SIZE << std::endl;
-    std::cout << "Looping Overhead With Loop of Size: " << LOOP_RUN << std::endl;
+    std::cout << "Looping Overhead Averaged Over: " << LOOP_RUN << std::endl;
     std::cout << multirun<long double>(for_loop_overhead, SAMPLE_SIZE) << " ns" << std::endl;
+
+    // Output Function Call Overhead
+    std::cout << "\nNumber of Runs: " << SAMPLE_SIZE << std::endl;
+    std::cout << "Function Call Overhead" << std::endl;
+    auto results = multirun_multiout<long double>(function_call_overhead, SAMPLE_SIZE);
+    for(unsigned int i = 0; i < results.size(); ++i) {
+        std::cout << "Number Arguments " << i << ": " << results[i] << " ns" << std::endl;
+    }
 }
